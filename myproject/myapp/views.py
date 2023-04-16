c6cde2f28e47118ce8f91d826bd1a0e334e0284f
tejaswibiduru1997 <tejaswibiduru22@gmail.com>
2023-04-16 17:01:00 +0530

    create a book object

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -6,7 +6,7 @@ from .models import Book
 from .serializers import BookSerializer
 
 # Create your views here.
-
+@csrf_exempt
 def book_list(request):
     if request.method == 'GET':
         books = Book.objects.all()
@@ -19,3 +19,25 @@ def book_list(request):
             serializer.save()
             return JsonResponse(serializer.data, status = 201)
         return JsonResponse(serializer.errors, status = 400)
+
+@csrf_exempt
+def book_detail(request, pk):
+    try:
+        book = Book.objects.get(pk=pk)
+    except Book.DoesNotExist:
+        return JsonResponse({'error': 'Book not found'}, status=404)
+    
+    if request.method == 'GET':
+        serializer = BookSerializer(book)
+        return JsonResponse(serializer.data)
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = BookSerializer(book, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status = 400)
+    elif request.method == 'DELETE':
+        book.delete()
+        return JsonResponse({'message':'Book was deleted successfully '}, status=204)
+    
